d51cbdca41fb4e7bca13339c309c067372861438
David Green <drgreen@gmail.com>
2022-12-12 09:19:39 -0600

    2015 day 12

--- 2015/day12.py
+++ 2015/day12.py
@@ -4,32 +4,56 @@ import re
 import json
 
 def readFile(path):
+    if not os.getcwd().endswith('2015'): os.chdir('2015')
     f = open(path, "r")
     return f.read().strip()
 
 def menu():
     main = "\nPlease choose an input option:\n"
-    main += "1. Text File\n"
-    main += "2. Prompt\n"
-    main += "3. Quit\n"
+    main += "1. Sample File\n"
+    main += "2. Input File\n"
+    main += "3. Other File\n"
+    main += "4. Prompt\n"
+    main += "5. Quit\n"
     main += ">> "
-    choice = input(main)
-    if choice == "3":
-        sys.exit(0)
-    if choice not in ["1","2"]:
-        print("Invalid option. Try Again.\n")
-        return menu()
-    else:
-        if choice == "1":
+    
+    filename = os.path.basename(__file__)
+    samplePath = filename.replace('.py', 'sample.txt')
+    inputPath = filename.replace('.py', 'input.txt')
+    
+    match input(main):
+        case "5":
+            sys.exit(0)
+        case "1":
+            return readFile(samplePath)
+        case "2":
+            return readFile(inputPath)
+        case "3":
             return readFile(input("File Name: "))
-        else:
+        case "4":
             return input("Input: ")
+        case _:
+            print("Invalid option. Try Again.\n")
+            return menu()
 
-def findRed(dict):
-    reg = re.compile(r'{([^}]*)}') #r'({[^{}]*red[^{}]*})')
-    input = re.sub(reg, '', input)
-    for k in dict.keys():
-        print('{}={}'.format(k, dict[k]))
+def part1(input):
+    reg = re.compile(r'(-?[\d]+)')
+    sum = 0
+    for num in re.findall(reg, input):
+        sum += int(num)
+    return sum
+
+def eval_obj_without_red(match):
+    m = match.group()
+    if ':"red"' not in m:
+        return str(part1(m))
+    else:
+        return str(0)
+
+def part2(input):
+    while ':"red"' in input:
+        input = re.sub(r"{[^{}]*}", eval_obj_without_red, input)
+    return part1(input)
 
 def getAnswer(input):
     if '\r' in input or '\n' in input:
@@ -37,16 +61,8 @@ def getAnswer(input):
         input = ''.join(input)
     answer = ['',''] #part1, part2
     
-    reg = re.compile(r'(-?[\d]+)')
-    sum = 0
-    for num in re.findall(reg, input):
-        sum += int(num)
-    
-    j = json.loads(input)
-    findRed(j[0])
-
-    answer[0] = sum
-    answer[1] = ''
+    answer[0] = part1(input)
+    answer[1] = part2(input)
     return answer
 
 while(True):
