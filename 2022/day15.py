a5ae99dbfe077fa8b6580d3fbad3d73c0b6747a9
David Green <drgreen@gmail.com>
2022-12-15 06:06:37 -0600

    initial file for 2022 day 15

---
+++ 2022/day15.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+def menu():
+    main = "\nPlease choose an input option:\n"
+    main += "1. Sample File\n"
+    main += "2. Input File\n"
+    main += "3. Other File\n"
+    main += "4. Prompt\n"
+    main += "5. Quit\n"
+    main += ">> "
+    
+    filename = os.path.basename(__file__)
+    samplePath = filename.replace('.py', 'sample.txt')
+    inputPath = filename.replace('.py', 'input.txt')
+    
+    match input(main):
+        case "5":
+            sys.exit(0)
+        case "1":
+            return readFile(samplePath)
+        case "2":
+            return readFile(inputPath)
+        case "3":
+            return readFile(input("File Name: "))
+        case "4":
+            return input("Input: ")
+        case _:
+            print("Invalid option. Try Again.\n")
+            return menu()
+
+def readFile(path):
+    if not os.getcwd().endswith('2022'): os.chdir('2022')
+    f = open(path, "r")
+    return f.read().strip()
+
+def getAnswer(input):
+    input = input.replace("\r", "").split("\n")
+    answer = [0,0] #part1, part2
+
+
+    
+    return answer
+
+while(True):
+    ans = getAnswer(menu())
+    print('\nanswer: {}'.format(ans))
