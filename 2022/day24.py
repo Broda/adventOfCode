1d21fc9b1fa6b27592a6c9d4abd424be97527a7e
David Green <drgreen@gmail.com>
2022-12-28 14:47:50 -0600

    work on 2022 day 24

--- 2022/day24.py
+++ 2022/day24.py
@@ -47,26 +47,43 @@ def tupleAdd(t1, t2):
 def tupleSub(t1, t2):
     return (t1[0]-t2[0],t1[1]-t2[1])
 
+def tupleMult(t, scalar):
+    return (t[0]*scalar, t[1]*scalar)
+
+def tupleMod(t, mod):
+    return (t[0]%mod, t[1]%mod)
+
 class Blizzard:
     def __init__(self, loc, dir, gridSize) -> None:
         self.loc = loc
         self.dir = dir
         self.gridSize = gridSize
+        self.moveOffsets = {'^':(0,-1),'v':(0,1),'<':(-1,0),'>':(1,0)}
+
+    def checkValidLocation(self, loc):
+        if loc[0] <= 0: loc = (self.gridSize[0]-2, loc[1])
+        if loc[0] >= self.gridSize[0]-1: loc = (1, loc[1])
+        if loc[1] <= 0: loc = (loc[0], self.gridSize[1]-2)
+        if loc[1] >= self.gridSize[1]-1: loc = (loc[0], 1)
+        return loc
 
     def move(self):
-        match self.dir:
-            case '^':
-                self.loc = tupleAdd(self.loc, (0,-1))
-                if self.loc[1] <= 0: self.loc = (self.loc[0], self.gridSize[1]-2) 
-            case 'v':
-                self.loc = tupleAdd(self.loc, (0,1))
-                if self.loc[1] >= self.gridSize[1]-1: self.loc = (self.loc[0], 1)
-            case '<':
-                self.loc = tupleAdd(self.loc, (-1,0))
-                if self.loc[0] <= 0: self.loc = (self.gridSize[0]-2, self.loc[1])
-            case '>':
-                self.loc = tupleAdd(self.loc, (1,0))
-                if self.loc[0] >= self.gridSize[0]-1: self.loc = (1, self.loc[1])
+        offset = self.moveOffsets[self.dir]
+        self.loc = self.checkValidLocation(tupleAdd(self.loc, offset))
+
+    def projectLocation(self, numMoves):
+        offset = tupleMult(self.moveOffsets[self.dir], numMoves)
+        mod = self.gridSize[0]-1 if offset[1] == 0 else self.gridSize[1]-1
+        loc = tupleAdd(self.loc, offset)
+        modResult = tupleMod(loc, mod)
+        return modResult
+        # print('offset: {}, loc: {}, size: {}'.format(offset, loc, self.gridSize))
+
+        # if offset[1] == 0: # x dir
+        #     print('tupleMod({}, {}) = {}'.format(loc, self.gridSize[0]-1, tupleMod(loc, self.gridSize[0]-1)))
+        #     return tupleAdd(loc, tupleMod(loc, self.gridSize[0]-1))
+        # # y dir
+        # return tupleAdd(loc, tupleMod(loc, self.gridSize[1]-1))
 
     def __str__(self) -> str:
         return self.dir
@@ -114,7 +131,8 @@ def getAnswer(input, isSample) -> list:
     print('Start: {}, End: {}'.format(start, end))
     printGrid(grid, size_x, size_y)
 
-
+    print(grid[(1,1)][0].projectLocation(5), "should be (6,1)")
+    print(grid[(1,1)][0].projectLocation(7), "should be (2,1)")
 
     return answer
 
