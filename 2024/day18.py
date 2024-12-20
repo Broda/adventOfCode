4c9472409508bf343a1d676b73ef903d95a464a1
David Green <drgreen@gmail.com>
2024-12-20 06:22:29 -0700

    2024 started Day 18

---
+++ 2024/day18.py
@@ -0,0 +1,64 @@
+import os
+import sys
+
+def menu():
+    main = "\nPlease choose an input option:\n"
+    main += "1. Sample File\n"
+    main += "2. Input File\n"
+    main += "3. Other File\n"
+    main += "4. Prompt\n"
+    main += "5. Quit\n"
+    main += ">> "
+    
+    filename = os.path.basename(__file__)
+    samplePath = filename.replace('.py', 'sample.txt')
+    inputPath = filename.replace('.py', 'input.txt')
+    
+    match input(main):
+        case "5":
+            sys.exit(0)
+        case "1":
+            isSample = True
+            return readFile(samplePath), True
+        case "2":
+            return readFile(inputPath), False
+        case "3":
+            return readFile(input("File Name: ")), False
+        case "4":
+            return input("Input: "), False
+        case _:
+            print("Invalid option. Try Again.\n")
+            return menu()
+
+def readFile(path):
+    if not os.getcwd().endswith('2024'): os.chdir('2024')
+    f = open(path, "r")
+    return f.read().strip()
+
+def writeFile(path, s):
+    if not os.getcwd().endswith('2024'): os.chdir('2024')
+    f = open(path, 'w')
+    f.write(s)
+    f.close()
+
+def getPart1(input : list, isSample : bool = False) -> int:
+    boardSize = (71,71)
+    if isSample: boardSize = (7,7)
+
+    return 0
+
+def getPart2() -> int:
+    return 0
+
+def getAnswer(input, isSample) -> list:
+    input = [(int(c.split(",")[0]),int(c.split(",")[1])) for c in input.replace("\r", "").split("\n")]
+    answer = [0,0] #part1, part2
+
+
+    answer[0] = getPart1(input, isSample)
+    answer[1] = getPart2()
+    return answer
+
+while(True):
+    ans = getAnswer(*menu())
+    print('\nanswer: {}'.format(ans))
