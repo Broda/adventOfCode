f9eca8709d5035e9a188694ba679da5075feb1fc
David Green <drgreen@gmail.com>
2024-12-20 06:24:10 -0700

    2024 work on Day 6

--- 2024/day06.py
+++ 2024/day06.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import copy
 
 def menu():
     main = "\nPlease choose an input option:\n"
@@ -82,22 +83,59 @@ def countSpaces(board) -> int:
             if c == "X": sum += 1
     return sum
     
-def getPart1(board : list) -> int:
+def pathIsLoop(path : list) -> bool:
+    last = path[-1]
+    for m in range(len(path)-1):
+        if m == last:
+            return True
+    return False
+
+def getPart1(board : list, moveMem : list) -> int:
     currPos = findGuard(board)
+    currDir = board[currPos[1]][currPos[0]]
+    #moveMem = [(currDir, currPos)]
     while currPos[0] >= 0 and currPos[0] < len(board[0]) and currPos[1] >= 0 and currPos[1] < len(board):
+        currDir = board[currPos[1]][currPos[0]]
+        moveMem.append((currDir, currPos))
+        # save currDir/moveMem from previous in case next move goes off the board
         currPos = tryMoveGuard(board, currPos)
-    
+        
     return countSpaces(board)
 
-def getPart2() -> int:
-    return 0
+def getPart2(input : list, moveMemP1 : list) -> int:
+    count = 0
+    start = moveMemP1[0]
+    for p in range(1, len(moveMemP1)):
+        board = copy.deepcopy(input)
+        loc = moveMemP1[p][1]
+        if loc[0] == start[1][0] and loc[1] == start[1][1]:
+            #print("same")
+            continue
+        board[loc[1]][loc[0]] = "#"
+        print(f"start = {start}, setting board[{loc[1]}][{loc[0]}] = '#'")
+        currDir, currPos = start
+        pathMem = []#[start]
+        while currPos[0] >= 0 and currPos[0] < len(board[0]) and currPos[1] >= 0 and currPos[1] < len(board):
+            currDir = board[currPos[1]][currPos[0]]
+            pathMem.append((currDir, currPos))
+            if len(pathMem) > 1 and pathIsLoop(pathMem):
+                count += 1
+                print(count)
+                break
+            
+            print(f"trying to move guard from {currPos}")
+            currPos = tryMoveGuard(board, currPos)
+            
+
+    return count
 
 def getAnswer(input, isSample) -> list:
     input = [list(x) for x in input.replace("\r", "").split("\n")]
     answer = [0,0] #part1, part2
 
-    answer[0] = getPart1(input)
-    answer[1] = getPart2()
+    moveMemP1 = []
+    answer[0] = getPart1(copy.deepcopy(input), moveMemP1)
+    answer[1] = getPart2(copy.deepcopy(input), moveMemP1)
     return answer
 
 while(True):
